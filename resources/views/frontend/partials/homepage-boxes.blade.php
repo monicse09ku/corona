2467c0257d96ec85e82227d359a8c9134908f03c
monicse09ku <monicse09ku@yahoo.com>
2020-05-04 19:17:28 +0600

    design issue fix

--- resources/views/frontend/partials/homepage-boxes.blade.php
+++ resources/views/frontend/partials/homepage-boxes.blade.php
@@ -4,32 +4,30 @@
     <div class="section-content">
       <div class="row" data-margin-top="-90px">
         <div class="col-sm-12 col-md-4">
-          <div class="icon-box p-40 iconbox-theme-colored bg-white border-1px text-center">
+          <div class="icon-box p-40 iconbox-theme-colored bg-white border-1px text-center" style="min-height: 305px;">
             <a class="icon" href="#">
               <i class="flaticon-charity-world-in-your-hands font-48 font-weight-100"></i>
             </a>
-            <h4 class="text-uppercase mt-0">bKash</h4>
-            <p>01627621937 - Saidur Rahman <br> 01711433117 - Arafat Habib Akash <br> 01670695630 - 
-            Ahmed All Razi</p>
+            <h4 class="text-uppercase mt-0">Our Mission</h4>
+            <p>Is to register a volunteer platform that rejects political, religious, racial or ideological biases neither driven by desire for gain nor by bureaucracy that will continue to serve its beneficiaries.</p>
           </div>
         </div>
         <div class="col-sm-12 col-md-4">
-          <div class="icon-box p-40 iconbox-theme-colored bg-white border-1px text-center">
+          <div class="icon-box p-40 iconbox-theme-colored bg-white border-1px text-center" style="min-height: 305px;">
             <a class="icon" href="#">
               <i class="flaticon-charity-shaking-hands-inside-a-heart font-48 font-weight-100"></i>
             </a>
-            <h4 class="text-uppercase mt-0">Rocket</h4>
-            <p>017238735146 - Apurba Kumar Sen <br> 016706956301 - Ahmed All Razi <br> 013031547524 - 
-            Niloy Sarkar</p>
+            <h4 class="text-uppercase mt-0">Our Vision</h4>
+            <p>Is to provide help towards those who need it most and divert all our strength for the betterment of the under-privileged without any discrimination.</p>
           </div>
         </div>
         <div class="col-sm-12 col-md-4">
-          <div class="icon-box p-40 iconbox-theme-colored bg-white border-1px text-center">
+          <div class="icon-box p-40 iconbox-theme-colored bg-white border-1px text-center" style="min-height: 305px;">
             <a class="icon" href="#">
               <i class="flaticon-charity-child-hand-on-adult-hand font-48 font-weight-100"></i>
             </a>
-            <h4 class="text-uppercase mt-0">DBBL</h4>
-            <p>Shounok Rahman, <br> AC. Info: 120.101.309440, <br> Khulna Branch</p>
+            <h4 class="text-uppercase mt-0">Our Goal</h4>
+            <p>Is to address the human suffering by helping people in need thus advocating humanitarian principals.</p>
           </div>
         </div>
       </div>
